03912fa233bf74e9a0be69a115663c2daa18e734
mdshahjhan <mdshahjha478877@gmail.com>
2022-10-12 13:04:28 +0600

    purpose

--- src/Component/Blog.jsx
+++ src/Component/Blog.jsx
@@ -6,9 +6,9 @@ const Blog = () => {
             <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
+      
+        Whai is reat Route Purpose
+      
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
