f556d727b7f456bc32255a967fe99f80e91a64bf
mdshahjhan <mdshahjha478877@gmail.com>
2022-12-10 20:15:23 +0600

    new model

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,33 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
+      <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src="https://img.freepik.com/premium-vector/quiz-vector-pop-style-illustration-with-megaphone-yellow-background_194782-902.jpg?w=740" className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-            <section>
-      <div className="bg-gray-100">
-        <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
-          <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-800">
-            Eduction start quick
-          </h1>
-          <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-900">
-            Best E-commerce platform for buying high quality Smart Home
-            Appliances at extremely affordable Price.
-          </p>
-          <div className="flex flex-wrap justify-center">
-            <Link to="/shop">
-             
-            </Link>
-           
-          </div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">Ther is a readu in interview question ?? is I am ok in interview question</p>
+          <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
-      <img
-       
-        alt=""
-        className="w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 bg-gray-500"
-      />
-    </section>
-            
-        </div>
+    </div>
     );
 };
 
